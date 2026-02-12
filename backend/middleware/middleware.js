ae52cb4a8d4fe5a1c1d847a2339a05c3fc59b565
Manikanta Potnuru <potnurumanikanta777@gmail.com>
2026-02-12 15:43:01 +0530

    "middleware addded"

---
+++ backend/middleware/middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
+
+export const authinticateToken = async (req, res, next) => {
+    let jwt_token
+    const authHeader = req.headers["authorization"]
+    if (authHeader !== undefined) {
+        jwt_token = authHeader.split(" ")[1]
+    }
+    if (jwt_token === undefined) {
+        return res.status(401).json({ error: "Invalid access token" })
+    } else {
+        jwt.verify(jwt_token, process.env.JWT_SECRET, async (error, payload) => {
+            if (error) {
+                return res.status(401).json({ error: "Invalid access token" })
+            } else {
+                request.username = payload.username;
+                next();
+            }
+        });
+    }
+};
