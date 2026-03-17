fab6a0442fc859aa1dcf7515e9b1f78c9427047a
Manikanta Potnuru <potnurumanikanta777@gmail.com>
2026-03-17 11:37:00 +0530

    "changes in about page"

--- frontend/src/components/AboutPage/AboutPage.jsx
+++ frontend/src/components/AboutPage/AboutPage.jsx
@@ -3,9 +3,9 @@ import Navbar from "../Navbar/Navbar"
 const AboutPage = () => (
     <>
     <Navbar/>
-    <div className="flex flex-col justify-center items-center min-h-screen">
-        <h1 className="text-blue-500 text-4xl font-sans">This is Ai powered course viewer application</h1>
-        <p className="text-black font-3xl font-mono">Explore all the courses</p>
+    <div className="flex flex-col justify-center items-center min-h-screen p-3 gap-2 bg-gradient-to-br from-slate-950 via-blue-950 to-slate-900">
+        <h1 className="text-white text-3xl font-sans font-bold">This is Ai powered course viewer application</h1>
+        <p className="text-gray-400 font-3xl font-mono">Explore all the courses</p>
     </div>
     </>
 )
